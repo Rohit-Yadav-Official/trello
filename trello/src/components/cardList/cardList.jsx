30f68314af5435fac8f7964bbdd4d46b429774b2
Rohit-Yadav-Official <roadtocode76448@gmail.com>
2025-04-05 17:45:08 +0530

    added some changes as per requirements

--- trello/src/components/cardList/cardList.jsx
+++ trello/src/components/cardList/cardList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "../card/card";
 import CardHeader from "../card header/cardHeader";
 import AddNewCard from "../addnewCard/addnewCard";
-const CardList = ({ cardata, title }) => {
+const CardList = ({ title,cardata }) => {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
       <CardHeader title={title} />
